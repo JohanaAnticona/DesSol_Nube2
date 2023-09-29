517f2dba963b23a151a2732f81f4b373ae39a70a
JohanaAnticona <113376013+JohanaAnticona@users.noreply.github.com>
2023-09-28 22:25:05 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "containers-us-west-98.railway.app";
+	$port = "7753";
+	$dbname = "railway";
+	$user = "postgres";
+	$password = "CT2frtuzLJ087lRr8z9P";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
